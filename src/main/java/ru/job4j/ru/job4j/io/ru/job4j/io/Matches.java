cf27204621ea96977b73d2e5e8463542649fe235
ignatown <ignatow.n@mail.ru>
2020-12-26 20:14:01 +0300

    FIX 2. Scanner и чтение числа из консоли. [#424648]

--- src/main/java/ru/job4j/ru/job4j/io/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/ru/job4j/io/ru/job4j/io/Matches.java
@@ -13,26 +13,27 @@ public class Matches {
     public static boolean victoryCheck(int match, int minus) {
         return (minus < 0 && minus > 4 && match - minus > -1);
     }
+    public static void nextMove(int nextPlayer, int actualPlayer, int match, int minus) {
+        do {
+            System.out.println("Ходит игрок № "+actualPlayer+"! Сколько берем спичек? (от 1 до 3)");
+            Scanner input = new Scanner(System.in);
+            minus = Integer.valueOf(input.nextLine());
+            possibleMove(match,minus,nextPlayer);
+        }
+        while (victoryCheck(match,minus));
+    }
 
     public static void main(String[] args) {
         int match = 11;
         int winnerFound = 0;
-        int mode;
-        boolean run = true;
-        int ans;
         Scanner input = new Scanner(System.in);
         System.out.println("Игра \"11\"!");
         match = 11;
         winnerFound = 0;
+        int minus=0;
         while (match > 0 && winnerFound == 0) {
-            int minus;
             System.out.println("Оставшихся спичек: " + match);
-            do {
-                System.out.println("Ходит первый игрок! Сколько берем спичек? (от 1 до 3)");
-                minus = Integer.valueOf(input.nextLine());
-                possibleMove(match,minus,2);
-            }
-            while (victoryCheck(match,minus));
+            nextMove(2, 1, match, minus);
             match -= minus;
             if (match == 0) {
                 System.out.println("Победил первый игрок.");
@@ -40,12 +41,7 @@ public class Matches {
             }
             if (winnerFound == 0) {
                 System.out.println("Оставшихся спичек: " + match);
-                do {
-                    System.out.println("Ходит второй игрок! Сколько берем спичек? (от 1 до 3)");
-                    minus = Integer.valueOf(input.nextLine());
-                    possibleMove(match,minus,1);
-                }
-                while (victoryCheck(match,minus));
+                nextMove(1, 2, match, minus);
                 match -= minus;
                 if (match == 0) {
                     System.out.println("Победил второй игрок.");
