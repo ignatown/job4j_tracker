3667d6f704347949538e4dfd219791c9d9e86cdd
ignatown <ignatow.n@mail.ru>
2020-12-27 03:05:11 +0300

    3.2. Зачем нужно приведение типов [#424657]

---
+++ src/main/java/ru/job4j/oop/vehicles/Transport.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop.vehicles;
+
+public class Transport {
+    public static void main(String[] args) {
+        Vehicle pl1 = new Plane();
+        Vehicle pl2 = new Plane();
+        Vehicle trn = new Train();
+        Vehicle bus = new Bus();
+
+        Vehicle[] vehicles = new Vehicle[]{pl1,pl2,trn,bus};
+
+        for (Vehicle a : vehicles) {
+            a.move();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/oop/vehicles/Vehicle.java
@@ -0,0 +1,5 @@
+package ru.job4j.oop.vehicles;
+
+public interface Vehicle {
+    public void move();
+}
