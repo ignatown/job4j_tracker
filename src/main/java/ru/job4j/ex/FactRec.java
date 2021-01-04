6a2629924ef0d063ef0172f0e5914f57da265d8e
ignatown <ignatow.n@mail.ru>
2021-01-05 00:35:49 +0300

    FIX 0.6. Error - исключения, связанные с работой виртуальной машины [#424671]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -5,9 +5,7 @@ public class FactRec {
         if (n == 0 || n == 1) {
             return 1;
         }
-        else {
-            return calc(n - 1) * n;
-        }
+        return calc(n - 1) * n;
     }
 
 
