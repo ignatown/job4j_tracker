77fef4be868794167ffc8586c141f9304d966f46
ignatown <ignatow.n@mail.ru>
2021-01-19 03:43:21 +0300

    3. Что такое JavaDoc [#424613]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу системы банковских переводов
+ * @author Никита Игнатов
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Для хранения данных о пользователе используется коллекция HashMap,
+     * где ключ - это класс User, а значение - это коллекция List.
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод добавляет нового пользователя в систему.
+     * Если предложенный к добавлению пользователь уже
+     * имеется в коллекции, то ничего не происходит.
+     * @param user пользователь, который добавляется в системую
+     */
     public void addUser(User user) {
     users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Метод добавляет новый аккаунт найденному по паспорту пользователю (если он существует)
+     * @param passport ключ, который используется для поиска в системе
+     *                 пользователя, которому будет добавлен аккаунт.
+     * @param account аккаунт, который будет добавлен пользователю.
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -21,6 +42,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод позволяющий найти пользователя по паспорту
+     * @param passport значение паспорта, по которому будет найден пользователь
+     * @return возвращает найденого пользователя или null, если пользователь не найден.
+     */
     public User findByPassport(String passport) {
         for (User us
                 : users.keySet()) {
@@ -31,6 +57,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяющий найти аккаунт пользователя по реквизитам (если он существует)
+     * @param passport паспорт, по которому определяется пользователь
+     * @param requisite реквизиты, по которым идет поиск у определенного пользователя
+     * @return возвращается аккаунт пользователя или null, если он не найден
+     */
     public Account findByRequisite(String passport, String requisite) {
         if (findByPassport(passport) != null) {
             for (Account acc
@@ -43,6 +75,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Метод позволяющий переводить средства с одного счета на другой между одним и разными пользователями
+     * если пользователи будут найдены.
+     * @param srcPassport паспорт пользователя, чьи средства списываются
+     * @param srcRequisite реквизиты аккаунта пользователя, с которого списываются средства
+     * @param destPassport паспорт пользователя, которому зачисляются средства
+     * @param destRequisite реквизиты аккаунта пользователя, на который зачисляются средства
+     * @param amount число средств, которые необходимо перевести между аккаунтами
+     * @return возвращает true, если перевод совершен и false, если перевод совершить не удалось
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
