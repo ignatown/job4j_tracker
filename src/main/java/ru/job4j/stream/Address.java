7dfc59d050c7f7d08564246b74ad7f25072bb802
ignatown <ignatow.n@mail.ru>
2021-01-27 01:54:29 +0300

    2. Список адресов [#424460]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+public class Address {
+    private String city;
+    private String street;
+    private int home;
+    private int apartment;
+Address(String city,String street, int home, int apartment){
+    this.city = city;
+    this.street = street;
+    this.home = home;
+    this.apartment = apartment;
+}
+}
---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    Profile(){
+    }
+
+    Profile(String city, String street, int home, int apartment){
+    super();
+    }
+}
