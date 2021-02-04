b442aae428dc94adcaddd030f75b06fecd32865c
ignatown <ignatow.n@mail.ru>
2021-02-04 15:01:41 +0300

    чекстайл

--- src/main/java/ru/job4j/stream/Tuple.java
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -6,19 +6,23 @@ public class Tuple {
     private String name;
     private double score;
 
-    public double getScore() {
-        return score;
-    }
-
     public Tuple(String name, double score) {
         this.name = name;
         this.score = score;
     }
 
+    public double getScore() {
+        return score;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Tuple tuple = (Tuple) o;
         return Double.compare(tuple.score, score) == 0 && Objects.equals(name, tuple.name);
     }
