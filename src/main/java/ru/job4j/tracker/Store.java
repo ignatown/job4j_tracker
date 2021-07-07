ac9a81bd574065ab1fae010530095e82aaee229b
ignatown <ignatow.n@mail.ru>
2021-07-07 17:55:19 +0300

    2. Трекер SQL [#1734 #174591]

--- src/main/java/ru/job4j/tracker/Store.java
+++ src/main/java/ru/job4j/tracker/Store.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public interface Store extends AutoCloseable {
     void init();
-    Item add(Item item) throws SQLException;
-    boolean replace(int id, Item item) throws SQLException;
-    boolean delete(int id) throws SQLException;
-    List<Item> findAll() throws SQLException;
-    List<Item> findByName(String key) throws SQLException;
-    Item findById(int id) throws SQLException;
+    Item add(Item item);
+    boolean replace(int id, Item item);
+    boolean delete(int id);
+    List<Item> findAll();
+    List<Item> findByName(String key);
+    Item findById(int id);
 }
--- src/main/java/ru/job4j/tracker/UserAction.java
+++ src/main/java/ru/job4j/tracker/UserAction.java
@@ -5,5 +5,5 @@ import java.sql.SQLException;
 public interface UserAction {
     String name();
 
-    boolean execute(Input input, Store memTracker) throws SQLException;
+    boolean execute(Input input, Store memTracker);
 }
